3fa8118c0ef5feb56a6bd0a2beb5ee730e52975e
patansharukhan <patansharu654321@gmail.com>
2023-08-15 00:48:31 +0530

    added confirmable migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_11_165644) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_14_191651) do
   create_table "foods", force: :cascade do |t|
     t.string "name"
     t.integer "price"
@@ -46,6 +46,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_11_165644) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "user_role"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
