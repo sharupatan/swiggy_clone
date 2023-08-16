10597e933931918ed868598061ab523692df80b4
patansharukhan <patansharu654321@gmail.com>
2023-08-17 00:32:22 +0530

    added cart model and applied associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_11_165644) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_16_185657) do
+  create_table "carts", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_carts_on_user_id"
+  end
+
   create_table "foods", force: :cascade do |t|
     t.string "name"
     t.integer "price"
@@ -50,5 +57,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_11_165644) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "carts", "users"
   add_foreign_key "foods", "restaurants"
 end
