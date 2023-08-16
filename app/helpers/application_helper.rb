12cb7ec6b70113acee75865b53ef06eaaf74c86d
patansharukhan <patansharu654321@gmail.com>
2023-08-17 01:04:49 +0530

    displayed count in ui

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
 	def restaurant_names
 		@restaurant_names = Restaurant.all.map{ |item| [item.name,item.id] }
 	end
+
+	def cart_items_count
+		current_user.cart ? current_user.cart.length : 0
+	end
 end
